db91953501d9c6335c49b013caa87dc9cb2de658
Dario Petrillo <dp1@users.noreply.github.com>
2017-12-06 09:00:00 +0100

    Create day6.5.py

---
+++ day6.5.py
@@ -0,0 +1,16 @@
+data = "2	8	8	5	4	2	3	1	5	5	1	2	15	13	5	14"
+data = [int(x) for x in data.split('\t')]
+
+res, t = 0, 0
+v = {}
+while tuple(data) not in v:
+	v[tuple(data)] = t
+	res += 1
+	t += 1
+	i, m = max(enumerate(data), key=lambda p: p[1])
+	data[i] = 0
+	for _ in range(m):
+		i = (i + 1) % len(data)
+		data[i] += 1
+
+print t - v[tuple(data)]
