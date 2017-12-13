375ea8e8b96a2b12b180fa5ddf112adf6800a6ed
Dario Petrillo <dario.pk1@gmail.com>
2017-12-13 14:20:21 +0100

    Explaining hex grid

--- day11.5.py
+++ day11.5.py
@@ -1,6 +1,18 @@
 with open("day11.txt", "r") as fin:
 	data = fin.read().strip().split(',')
 
+# The hexagonal grid goes from this:
+#   a
+# b   c
+#   D
+# e   f
+#   g
+#
+# To this, being skewed to the side:
+# a c
+# b D f
+# e g
+
 ops = {'n': (-1, -1), 'nw': (0, -1), 'sw': (1, 0), 's': (1, 1), 'se': (0, 1), 'ne': (-1, 0)}
 
 x, y, res = 0, 0, 0
